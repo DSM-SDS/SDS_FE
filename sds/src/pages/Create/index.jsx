074f9f32a586f06426e5ae76bdaaac4f6b0c1849
downpour07 <choiheeyoung10@gmail.com>
2024-07-16 19:38:47 +0900

    📑 :: Create page 퍼블리싱

--- sds/src/pages/Create/index.jsx
+++ sds/src/pages/Create/index.jsx
@@ -1,36 +1,132 @@
-import React from "react";
-import "./Signup.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { styled } from "styled-components";
+import { color } from "../../styles/theme";
 
-const Signup = () => {
+import MainHeader from "../../components/MainHeader";
+
+const CreatePage = () => {
+  const onClickRemote = () => {
+    alert('신고 접수되었습니다.');
+  }
+  
+
   return (
-    <div className="container">
-      <div className="header">
-        <header>
-          <ul>
-            <li>로그인</li>
-            <li>회원가입</li>
-          </ul>
-        </header>
-      </div>
-      <div className="main">
-        <h2>SignUp</h2>
-        <div class="inputs">
-          <input className="name" type="text" placeholder="이름"></input>
-          <input className="id" type="text" placeholder="아이디"></input>
-          <input className="pw" type="password" placeholder="비밀번호"></input>
-          <input
-            className="pwcheck"
-            type="pasword"
-            placeholder="비밀번호 확인"
-          ></input>
-          <input
-            className="apartname"
-            type="text"
-            placeholder="아파트 이름"
-          ></input>
-        </div>
-        <button>Signup</button>
-      </div>
-    </div>
+    <>
+      <MainHeader />
+      <Container>
+        <Text>신고하기</Text>
+        <TopDiv>
+          <InputDiv>
+            <BoldText>날짜</BoldText>
+            <TopInput placeholder="발생한 날짜를 입력해주세요 ex)20240716"/>
+          </InputDiv>
+          <InputDiv>
+            <BoldText>시간</BoldText>
+            <TopInput placeholder="발생한 시간을 입력해주세요 ex)10:30 ~ 11:50"/>
+          </InputDiv>
+        </TopDiv>
+        <InputDiv>
+          <BoldText>제목</BoldText>
+          <Input placeholder="제목을 입력해주세요"/>  
+        </InputDiv>
+        <InputDiv>
+          <BoldText>신고 내용</BoldText>
+          <ContentsInput placeholder="신고 내용을 입력해주세요"/>
+        </InputDiv>
+        <Button onClick={() => onClickRemote()}>신고하기</Button>
+      </Container>
+    </>
   );
 };
+
+const Container = styled.div`
+	width: 100%;
+	height: 94vh;
+  background-color: ${color.Gray[0]};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  row-gap: 50px;
+`;
+
+const Text = styled.div`
+  font-size: 40px;
+  font-weight: bold;
+  color: ${color.Red[1]};
+`
+
+const TopDiv = styled.div`
+  display: flex;
+  gap: 100px;
+`
+
+const BoldText = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${color.Black};
+`
+
+const Input = styled.input`
+  width: 930px;
+  height: 60px;
+  font-size: 16px;
+  font-weight: 500;
+  border-radius: 10px;
+  border: none;
+  padding-left: 30px;
+  &:focus {
+    outline: none;
+  }
+`
+
+const TopInput = styled.input`
+  width: 400px;
+  height: 60px;
+  font-size: 16px;
+  font-weight: 500;
+  border-radius: 10px;
+  border: none;
+  padding-left: 30px;
+  &:focus {
+    outline: none;
+  }
+`
+
+const InputDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+`
+export const ContentsInput = styled.textarea`
+  width: 930px;
+  height: 200px;
+  border-radius: 10px;
+  padding: 15px 20px;
+  border: 1px solid #e1e2e3;
+  color: #191c1b;
+  display: flex;
+  resize: none;
+  font-size: 16px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Button = styled.button`
+  background-color: ${color.Red[1]};
+  color: ${color.White};
+  font-size: 20px;
+  font-weight: 600;
+  width: 970px;
+  height: 60px;
+  border: none;
+  border-radius: 10px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+export default CreatePage;
