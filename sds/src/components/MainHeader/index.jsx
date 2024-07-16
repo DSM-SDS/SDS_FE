1102a074fe418e4d62cddc9d97fb97362e7e450d
downpour07 <choiheeyoung10@gmail.com>
2024-07-16 19:37:17 +0900

    📑 :: Header 퍼블리싱

--- sds/src/components/MainHeader/index.jsx
+++ sds/src/components/MainHeader/index.jsx
@@ -5,15 +5,12 @@ import { color } from "../../styles/theme";
 
 import Logo from "../../assets/icon/Logo";
 
-function Header() {
+function MainHeader() {
 
   return (
     <Container>
       <Logo />
-      <ButtonDiv>
-        <Button>로그인</Button>
-        <Button>회원가입</Button>
-      </ButtonDiv>
+      <ButtonDiv></ButtonDiv>
     </Container>
   );
 }
@@ -27,24 +24,8 @@ const Container = styled.div`
   justify-content: space-around;
 `;
 
-const Button = styled.button`
-  width: 100px;
-  height: 60px;
-  align-items: center;
-  justify-content: center;
-  border: none;
-  background-color: ${color.White};
-  font-size: 16px;
-  font-weight: 500;
-
-  &:hover {
-    cursor: pointer;
-  }
-`
-
 const ButtonDiv = styled.div`
   width: 220px;
   height: 60px;
 `
-
-export default Header;
+export default MainHeader;
